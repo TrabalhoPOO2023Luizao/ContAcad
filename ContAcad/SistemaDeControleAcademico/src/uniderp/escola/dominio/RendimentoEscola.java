c75ee67f9c078ab5c783c818a5f2d90b654dbd24
Guilherme <eberrn@hotmail.com>
2023-10-22 14:52:03 -0400

    Relação de Herança Rendimento Escolar
    Correção de relação de herança no Domínio Rendimento Escolar que estava incorreta.

--- ContAcad/SistemaDeControleAcademico/src/uniderp/escola/dominio/RendimentoEscola.java
+++ ContAcad/SistemaDeControleAcademico/src/uniderp/escola/dominio/RendimentoEscola.java
@@ -2,7 +2,7 @@ package uniderp.escola.dominio;
 
 import java.util.ArrayList;
 
-public class RendimentoEscola {
+public class RendimentoEscola extends BaseIdentificador {
     private Aluno aluno;
     private Turma turma;
     private double notaPrimeiraProva;
@@ -16,12 +16,14 @@ public class RendimentoEscola {
         for(NotaTrabalho trabalho : trabalhos){
             soma += trabalho.getNota();
         }
-        return soma / trabalhos.size();
+        this.mediaTrabalhos = soma / trabalhos.size();
+        return this.mediaTrabalhos;
     }
     
     private double calcularMediaGeral(){
         double media = (notaPrimeiraProva + notaSegundaProva) / 2.0;
-        return (media + mediaTrabalhos) / 2.0;
+        this.mediaGeral = (media + mediaTrabalhos) / 2.0;
+        return this.mediaGeral;
     }
 
     public Aluno getAluno() {
@@ -80,15 +82,18 @@ public class RendimentoEscola {
         this.mediaGeral = mediaGeral;
     }
 
-    public RendimentoEscola(Aluno aluno, Turma turma, double notaPrimeiraProva, double notaSegundaProva,
-            ArrayList<NotaTrabalho> trabalhos, double mediaTrabalhos, double mediaGeral) {
+    public RendimentoEscola(int codigo, Aluno aluno, Turma turma, double notaPrimeiraProva, double notaSegundaProva,
+            ArrayList<NotaTrabalho> trabalhos) {
+        super(codigo);
         this.aluno = aluno;
         this.turma = turma;
         this.notaPrimeiraProva = notaPrimeiraProva;
         this.notaSegundaProva = notaSegundaProva;
         this.trabalhos = trabalhos;
-        this.mediaTrabalhos = calcularMediaTrabalhos();
-        this.mediaGeral = calcularMediaGeral();
+        calcularMediaTrabalhos();
+        calcularMediaGeral();
     }
+
+    
     
 }
