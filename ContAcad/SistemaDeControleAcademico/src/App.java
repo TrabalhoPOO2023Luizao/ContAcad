3ee6175c8cb890eee3163fa4ff4280389b679b2c
eber <eberrn@hotmail.com>
2023-10-30 16:26:27 -0400

    mudanca app java
    removido codigo comentado

--- ContAcad/SistemaDeControleAcademico/src/App.java
+++ ContAcad/SistemaDeControleAcademico/src/App.java
@@ -15,22 +15,6 @@ import uniderp.escola.servico.AlunoServico;
 
 public class App {
     public static void main(String[] args) {
-        // AlunoServico serv = new AlunoServico();
-        // Aluno aluno = new Aluno(02, "Allan", LocalDate.of(2000, 02, 10), "allan 02", "1234", 2);
-
-        // serv.Inserir(aluno);
-
-        //     for (Aluno listaAluno : serv.Listar()) {
-        //     System.out.println("-----------------------------------");
-        //     listaAluno.Imprimir();
-        // }
-        // DisciplinaFakeDB disciplina = new DisciplinaFakeDB();
-
-        // TurmaFakeDB turma = new TurmaFakeDB();
-
-        // TrabalhoFakeDB trab = new TrabalhoFakeDB();
-
-
         Menu menu = new Menu();
         menu.Executar();
     }
